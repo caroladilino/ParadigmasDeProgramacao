dcb5f478cf760f29bbdfa6b5787a4cbc0759e5ea
Carolina Adilino <cmqadilino@gmail.com>
2026-03-19 23:52:33 -0300

    resolução do exercício 2422 do beecrowd

---
+++ 2422.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+N = int(input())
+casas=[]
+a=0
+b=0
+
+
+for i in range (N):
+    casas.append(int(input()))
+
+soma = int(input())
+
+checados = set()
+
+for num in casas:
+    complemento = soma - num
+    if complemento in checados:
+        print(complemento, num)
+        break
+    checados.add(num)
+
+
